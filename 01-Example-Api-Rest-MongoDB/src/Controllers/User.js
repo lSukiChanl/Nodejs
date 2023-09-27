6cd2bb9fcc9eb9e3b41c9a117730a187934f4da9
SukiChan <crtmg@outlook.com>
2023-09-27 09:11:19 -0600

    Agregando Metodos de Ejemplo

--- 01-Example-Api-Rest-MongoDB/src/Controllers/User.js
+++ 01-Example-Api-Rest-MongoDB/src/Controllers/User.js
@@ -10,6 +10,24 @@ const getAllUsers = async (req, res) => {
     }
 };
 
+const getUsuarioJson = async (req, res) => {
+  try {
+    const filtro = req.body; // JSON con el campo de búsqueda
+
+    // Modificar el filtro para realizar una búsqueda insensible a mayúsculas y minúsculas
+    for (const key in filtro) {
+      if (filtro.hasOwnProperty(key) && typeof filtro[key] === 'string') {
+        filtro[key] = { $regex: new RegExp(filtro[key], 'i') };
+      }
+    }
+    const usuariosEncontrados = await ModelUser.find(filtro);
+
+    res.json(usuariosEncontrados);
+  } catch (err) {
+    res.status(500).json({ error: 'Error al buscar usuarios' });
+  }
+};
+
 const getUserById = async (req, res) => {
     try {
         const User = await ModelUser.findById(req.params.id);
@@ -28,6 +46,68 @@ const getUserByUsername = async (req, res) => {
     }
 };
 
+const deleteUserById = async (req, res) => {
+  try {
+      const User = await ModelUser.findByIdAndDelete(req.params.id);
+      res.json(User);
+  } catch (err) {
+      res.status(500).json({ error: 'Error al obtener el Usuario' });
+  }
+};
+
+const deleteUserByUsername = async (req, res) => {
+  try {
+      const User = await ModelUser.deleteMany({ username: { $regex: req.params.id, $options: 'i'}});
+      res.json(User);
+  } catch (err) {
+      res.status(500).json({ error: 'Error al obtener el Usuario' });
+  }
+};
+
+const updateUserByIdRemove = async (req, res) => {
+    const { username, password } = req.body;
+    const userId = req.params.id;
+  
+    try {
+      const userToUpdate = await ModelUser.findById(req.params.id);
+  
+      if (!userToUpdate) {
+        return res.status(404).json({ error: 'Usuario no encontrado' });
+      }
+  
+      userToUpdate.username = username;
+      userToUpdate.password = password;
+  
+      const updatedUser = await userToUpdate.save();
+  
+      res.json(updatedUser);
+    } catch (err) {
+      res.status(500).json({ error: 'Error al actualizar el Usuario' });
+    }
+};
+
+
+const updateUserByIdKeep = async (req, res) => {
+    const usuarioId = req.params.id; // ID del usuario a actualizar
+    const updateData = req.body; // JSON con los campos a actualizar
+    
+    try {
+      const usuarioActual = await ModelUser.findById(usuarioId);
+    
+      if (!usuarioActual) {
+        return res.status(404).json({ error: 'Usuario no encontrado' });
+      }
+    
+      const usuarioActualizado = Object.assign(usuarioActual, updateData);
+    
+      const resultado = await usuarioActualizado.save();
+    
+      res.json(resultado);
+    } catch (err) {
+      res.status(500).json({ error: 'Error al actualizar el Usuario' });
+    }
+};
+
 const addUser = async (req, res) => {
     const { username, password } = req.body;
     const User = new ModelUser({ username, password });
@@ -40,4 +120,4 @@ const addUser = async (req, res) => {
     }
 };
 
-module.exports = {getAllUsers, getUserById, getUserByUsername, addUser};
+module.exports = {getAllUsers, getUsuarioJson, getUserById, getUserByUsername, deleteUserById, deleteUserByUsername, updateUserByIdRemove, updateUserByIdKeep, addUser};
--- 01-Example-Api-Rest-MongoDB/src/Route/User.js
+++ 01-Example-Api-Rest-MongoDB/src/Route/User.js
@@ -5,9 +5,19 @@ const router = express.Router();
 
     router.get('/users', UserController.getAllUsers);
 
-    router.get('/user/id/:id', UserController.getUserById);
+    router.get('/users/find', UserController.getUsuarioJson);
+
+    router.get('/user/:id', UserController.getUserById);
 
     router.get('/user/username/:id', UserController.getUserByUsername);
+
+    router.delete('/user/delete/:id', UserController.deleteUserById);
+
+    router.delete('/users/delete/:id', UserController.deleteUserByUsername);
+
+    router.put('/user/updateremove/:id', UserController.updateUserByIdRemove);
+
+    router.put('/user/updatekeep/:id', UserController.updateUserByIdKeep);
 
     router.post('/user/add', UserController.addUser);
 
