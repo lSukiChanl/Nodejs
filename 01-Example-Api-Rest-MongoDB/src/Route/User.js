0da93727378bf0ad83a1c66ab139b548866b2fac
SukiChan <crtmg@outlook.com>
2023-09-26 20:01:25 -0600

    Busqueda por Nombre

--- 01-Example-Api-Rest-MongoDB/src/Route/User.js
+++ 01-Example-Api-Rest-MongoDB/src/Route/User.js
@@ -3,12 +3,12 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/users', UserController.getAllUsers);
+    router.get('/users', UserController.getAllUsers);
 
-router.get('/user/id/:id', UserController.getUserById);
+    router.get('/user/id/:id', UserController.getUserById);
 
-router.get('/user/username/:id', UserController.getUserByUsername);
+    router.get('/user/username/:id', UserController.getUserByUsername);
 
-router.post('/user/add', UserController.addUser);
+    router.post('/user/add', UserController.addUser);
 
 module.exports = router;
--- 01-Example-Api-Rest-MongoDB/src/index.js
+++ 01-Example-Api-Rest-MongoDB/src/index.js
@@ -1,39 +1,26 @@
-// Importa las dependencias
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config();
 const UserRouter = require('./Route/User');
-
-// Carga las variables de entorno desde un archivo .env
-dotenv.config();
-
-// Crea una instancia de Express
 const app = express();
+const Conexion = `mongodb://${process.env.UserMongoDB}:${process.env.PassMongoDB}@${process.env.IpMongoDB}:${process.env.PortMongoDB}/${process.env.DBMongoDB}?authSource=admin`;
+const PORT = process.env.PORT || 3000;
 
-// Configura middleware
 app.use(bodyParser.json());
 app.use('/api', UserRouter)
 
-// Conecta a MongoDB usando Mongoose
-mongoose.connect('mongodb://SukiChan:SukiChan%232036MON@45.41.206.139:27017/Example?authSource=admin', {
+mongoose.connect(Conexion, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-  .then(() => {
-    console.log('Conexión a MongoDB exitosa');
-  })
-  .catch((err) => {
-    console.error('Error al conectar a MongoDB:', err);
-  });
-
-// Define rutas de la API
-app.get('/', (req, res) => {
-  res.send('¡Bienvenido a mi API REST!');
+.then(() => {
+  console.log('Conexión a MongoDB exitosa');
+})
+.catch((err) => {
+  console.error('Error al conectar a MongoDB:', err);
 });
 
-// Inicia el servidor en un puerto específico
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor en ejecución en el puerto ${PORT}`);
 });
